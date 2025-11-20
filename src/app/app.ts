f818e7f92529b2dd336bd4b0644ec8350bf18328
Dhanabalan D <dhana22busy@gmail.com>
2025-11-20 14:20:29 +0530

    Added categories

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { HeroComponent } from './components/hero/hero';
+import { CategoriesComponent } from './components/categories/categories';
 import { ProductsComponent } from './components/products/products';
 import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, ProductsComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, CategoriesComponent, ProductsComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
