4e6c86ec248f5abc0bff9feb71c83ea2c67a91a9
Dhanabalan D <dhana22busy@gmail.com>
2025-11-20 14:52:16 +0530

    Responsivness added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { HeroComponent } from './components/hero/hero';
 import { CategoriesComponent } from './components/categories/categories';
@@ -10,7 +9,7 @@ import { FooterComponent } from './components/footer/footer';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, CategoriesComponent, ProductsComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent, CategoriesComponent, ProductsComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
