e56357c5db3389f40143381d6610edce72b34363
Dhanabalan D <dhana22busy@gmail.com>
2025-11-24 22:52:05 +0530

    contact methods added

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,12 @@ import { CategoriesComponent } from './components/categories/categories';
 import { ProductsComponent } from './components/products/products';
 import { FooterComponent } from './components/footer/footer';
 
+import { ContactComponent } from './components/contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, HeroComponent, CategoriesComponent, ProductsComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent, CategoriesComponent, ProductsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
